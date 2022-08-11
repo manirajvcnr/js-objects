1c965f569f38cdc3df920d3618c473bc900b6ab2
manirajvcnr <cnrakashcnr@gmail.com>
2022-08-11 16:41:58 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,93 @@
+// car 
+let car={brand:'maruti',model:'swift',color:'white',fueltype:'diesel'};
+car.company=car.brand;
+console.log(car.company);
+console.log(car.brand);
+console.log(car['brand']);
+
+// Mobile
+let mobile={brand:'realme',model:'x2',camera:'64mp',color:'green',processor:'snapdragon',ram:'8gb'};
+
+// laptop
+let laptop={brand:'lenovo',model:'idea pad',ram:'8gb',processor:'i3-11th gen',color:'silver'};
+
+// pen
+let pen={brand:'renolds',type:'capped',ink:'blue',body:'glass'};
+
+// shirt
+let shirt={brand:'zero',varient:'plane',color:'blue',size:'xl',type:'fullhand'};
+
+// men
+let men={firstname:'mani',lastname:'raj',place:'chinnanadaroor',address:'3/150,north street,cnr',mobile:'8056663053'};
+
+// booking ticket
+let passanger={passangername:'maniraj',busno:'TN 01 L 8792',from:'pavoorchatram',to:'chennai',time:'6.30 pm',seatno:'7',seat:'non-sleeper'};
+
+// bike
+let bike={brand:'honda',model:'unicorn',number:'TN 72 L 2504',color:'Black',engineno:'12345',mileage:'40km'};
+
+// note
+let note={company:'notebook',size:'long',pages:'80',page:'no-line'};
+
+// headset
+let headset={brand:'boat',type:'wired',model:'bass 900',color:'black',varient:'boom'};
+
+// bag
+let bag={brand:'american tourister',color:'black',size:'college bag',capacity:'4',type:'back bag'};
+
+// house
+let house={place:'chennai',model:'apartment',size:'2bhk'};
+
+// gun
+let gun={model:'m1887',range:'short range',rateoffire:'0.1s',bullet:'2',weight:'3kg'};
+
+// fan
+let fan={brand:'usha',model:'123',type:'cealing',color:'brown',revolution:'1200/mins'};
+
+// watch
+let watch={brand:'fasttrack',type:'digital',color:'black',varient:'wrist',hrs:'12hrs'}
+
+// paper
+let paper={size:'a4',color:'white',line:'no line'};
+
+// frame
+let frame={height:'90cm',width:'90cm',border:'solid black',bacground:'white',photo:'color'};
+
+// table
+let table={height:'2m',width:'4m',type:'wood',polish:'polished',color:'brown'};
+
+// class room
+let classroom={row:'8',bench:'20',students:'50',boys:'30',girls:'20',classno:'10'};
+
+// school
+let school={name:'pahss',place:'avudayanoor',ph:'044 823',students:'1200',teachers:'40'};
+
+// eye class
+let eyeclass={frame:'black',lens:'0.3',type:'short',rate:'1500'};
+
+// hard drive
+let drive={storage:'1tb',color:'black',model:'lpddr4'};
+
+// hotel
+let hotel={available:'8am',close:'10pm',food:'veg/non veg',place:'pcm'};
+
+// chocolate
+let chocolate={flavour:'vennila',weight:'20g',brand:'arun'};
+
+// powerbank
+let powerbank={capacity:"20000mah",brand:'realme',cable:'type c',type:'20w fast charging'};
+
+// actor
+let actor={name:'vijay',movies:'66',lastmovie:'master',upcoming:'67'};
+
+// dts
+let dts={nam:'ram',seat:'500',place:'tirunelveli',booked:'420',available:'80'};
+
+// student
+let students={name:'abc',department:'cse',year:'2022',regno:'123456789'};
+
+// furniture
+let furniture={name:'fridge',capacity:'200ltr',color:'white',brand:'lg',height:'6feet'};
+
+// family
+let family={father:'abc',mother:'abc',brother:'abc',sister:'abc',members:'5'};
